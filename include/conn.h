e9d5f1048226578f416b8648fc15ab4598880b4b
Your <184955262+yourluckystar@users.noreply.github.com>
2025-04-16 08:12:11 +0900

    also dont need

--- include/conn.h
+++
@@ -1,7 +0,0 @@
-#ifndef CONN_H
-#define CONN_H
-
-int conn(struct pollfd *fds, int nfds, int i, int sockfd);
-void disconn(struct pollfd *fds, int nfds, int i);
-
-#endif
--- include/msg.h
+++
@@ -1,6 +0,0 @@
-#ifndef MSG_H
-#define MSG_H
-
-void broadcast(int sender, struct pollfd *fds, int nfds, char *msg, int len);
-
-#endif

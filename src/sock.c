9091f32f4ea2321351444e17c573ada552018029
Your <184955262+yourluckystar@users.noreply.github.com>
2025-04-16 08:14:04 +0900

    small changes

--- src/sock.c
+++ src/sock.c
@@ -4,6 +4,7 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
 #include "server.h"
@@ -18,45 +19,54 @@ setnonblocking(int sockfd)
 }
 
 int
-setup_sock(int port)
+setup_sock(int port,
+           int *sockfd)
 {
-	int sockfd, rv;
-	struct sockaddr_in addr;
+        struct sockaddr_in addr;
+	int rv;
 	int yes = 1;
 
-	(void)memset(&addr, 0, sizeof(addr));
+	memset(&addr, 0, sizeof(addr));
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(port);
 	addr.sin_addr.s_addr = INADDR_ANY;
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1) {
+	*sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (*sockfd == -1)
+        {
 		(void)printf("socket(2) failed: %d\n", errno);
 		return -1;
 	}
 
-	rv = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
-	if (rv == -1) {
+	rv = setsockopt(*sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+	if (rv == -1)
+        {
 		(void)printf("setsockopt(2) failed: %d\n", errno);
 		return -1;
 	}
 
-	rv = bind(sockfd, (struct sockaddr*)&addr, sizeof(addr));
-	if (rv == -1) {
+	rv = bind(*sockfd, (struct sockaddr*)&addr, sizeof(addr));
+	if (rv == -1)
+        {
 		(void)printf("bind(2) failed: %d\n", errno);
+                (void)close(*sockfd);
 		return -1;
 	}
 
-	rv = listen(sockfd, BACKLOG);
-	if (rv == -1) {
+	rv = listen(*sockfd, BACKLOG);
+	if (rv == -1)
+        {
 		(void)printf("listen(2) failed: %d\n", errno);
+                (void)close(*sockfd);
 		return -1;
 	}
 
-	rv = setnonblocking(sockfd);
-	if (rv == -1) {
+	rv = setnonblocking(*sockfd);
+	if (rv == -1)
+        {
 		(void)printf("fcntl(2) failed: %d\n", errno);
 		return -1;
 	}
-	return sockfd;
+
+	return 0;
 }
